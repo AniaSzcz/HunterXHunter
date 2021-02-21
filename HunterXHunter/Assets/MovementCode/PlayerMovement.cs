e71f77a3bbd2560c4359a0875158268979e270dc
AniaS <aniaszczeszynski@gmail.com>
2021-02-21 17:12:28 -0500

    Beginning Stages of Climbing State
    Press e when getting near/touching the pole to not be able to walk anymore

--- HunterXHunter/Assets/MovementCode/PlayerMovement.cs
+++ HunterXHunter/Assets/MovementCode/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Walking ---------------------------------------------------------
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
@@ -13,9 +14,40 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     Vector3 velocity;
     bool isGrounded;
+    // Climbing ---------------------------------------------------------
+    bool touchingTree;
+    public Transform treeDetection;
+    public LayerMask treeMask;
+    bool climbingState = false;
+    // State int --------------------------------------------------------
+    int state = 0;
     
     // Update is called once per frame
     void Update()
+    {
+        // Climbing state -----------------------------------------------
+        // Drawing an invisible circle around the empty TreeDetection and seeing when that collides with the tree
+        touchingTree = Physics.CheckSphere(treeDetection.position, 1, treeMask);
+        // If it's touching, go into the climbing state
+        if(touchingTree == true && Input.GetKey(KeyCode.E))
+        {
+            state = 1;
+        } 
+        
+        
+
+        
+        
+        if (state == 0)
+        {
+            Walking();
+        }
+        else if (state == 1)
+        {
+            Climbing();
+        }
+    }
+    void Walking()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
@@ -37,9 +69,10 @@ public class PlayerMovement : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
 
-        Debug.Log(velocity.y);
-
         controller.Move(velocity * Time.deltaTime);
-    
+    }
+    void Climbing()
+    {
+        
     }
 }
